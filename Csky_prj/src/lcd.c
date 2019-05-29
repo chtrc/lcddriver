77f5c3516b7156f8386621bb507a7ea92c324f5e
lustrew <229559117@qq.com>
2019-05-29 15:59:19 +0800

    arrange

--- Csky_prj/src/lcd.c
+++ Csky_prj/src/lcd.c
@@ -1,86 +1,75 @@
-
 #define LCD_AXIs_VIDEOMEM_ADDR     0 
 #define LCD_AXIs_VIDEOMEM_LENGTH   1 
 
 #define LCD_AXIs_LCDINITCMD_ADDR   2 
 #define LCD_AXIs_LCDINITCMD_LENGTH 3 
 
-#define LCD_AXIs_LCD_CTRL          4 
+//[0]=0 send data; [0]=1, initial; 
+#define LCD_AXIs_LCD_CTRL          4
+
 #define LCD_AXIs_LCD_STATUS        5 
 
+#define INITIAL   0
+#define SEND_DATA 1
 
 unsigned int* LCD_AXI_MASTER_BASEADDR = (unsigned int*)0x8200_0000;
 unsigned int* LCD_AXI_SLAVE_BASEADDR  = (unsigned int*)0x44A0_0000;
 
-void delay(){
-  short i,j = 0;
-  for(i = 0;i < 100000;i++)
-  {
-    j++;
+
+// 64 bytes, 32 bits/per
+// 64*8/32 = 16 integers per time
+void write_lcd(int ctl, int *wdata, int length, int type){
+  switch(type){
+    case INITIAL:
+      LCD_AXI_SLAVE_BASEADDR[LCD_AXIs_LCD_CTRL] = ctl;
+      LCD_AXI_SLAVE_BASEADDR[LCD_AXIs_LCDINITCMD_ADDR] = wdata;
+      LCD_AXI_SLAVE_BASEADDR[LCD_AXIs_LCDINITCMD_LENGTH] = length;
+      break;
+    case SEND_DATA:
+      LCD_AXI_SLAVE_BASEADDR[LCD_AXIs_LCD_CTRL] = ctl;
+      LCD_AXI_SLAVE_BASEADDR[LCD_AXIs_VIDEOMEM_ADDR] = wdata;
+      LCD_AXI_SLAVE_BASEADDR[LCD_AXIs_VIDEOMEM_LENGTH] = length;
+      break;
   }
 }
-
-
-void write_lcd(unsigned char ctl,unsigned char wdata){
-  CTL = ctl;
-  LCD_BASE_ADDR[...] = wdata;
-}
-
 
 int main() {
- unsigned int DBL[]={
-                        0xff,0xff,0x98,0x06,
-                        0x2a,0x00,0x00,0x03,0x20,
-                        0x2b,0x00,0x00,0x01,0xe0,
-                        0x2c
-                     };
-  short i,j;
+  // command
+  int cmd[] = {
+    0xff,0xff,0x98,0x06,
+    0x2a,0x00,0x00,0x03,0x20,
+    0x2b,0x00,0x00,0x01,0xe0,
+    0x2c
+  };
+  
+  // lcd data
+  int data[] = {
+    0xff00,0xff00,0xff00,0xff00,
+    0x0ff0,0x0ff0,0x0ff0,0x0ff0,
+    0x00ff,0x00ff,0x00ff,0x00ff,
+    0xf00f,0xf00f,0xf00f,0xf00f
+  };
   
   // initialize
-  write_lcd(0b0110,DBL[0]);
-  for(i=1;i<=3;i++){
-    write_lcd(0b0111,DBL[i]);
-  }
-
-
+  write_lcd(0b0110,cmd[0],1,INITIAL);  
+  write_lcd(0b0111,cmd[1],3,INITIAL);
+  
   // config window's size
-  write_lcd(0b0110,DBL[4]);
-  for(i=5;i<=8;i++){
-    write_lcd(0b0111,DBL[i]);
-  }
-  
-  write_lcd(0b0110,DBL[9]);
-  for(i=10;i<=13;i++){
-    write_lcd(0b0111,DBL[i]);
-  }
+  // 800
+  write_lcd(0b0110,cmd[4],1,INITIAL);
+  write_lcd(0b0111,cmd[5],4,INITIAL);
+  // 480
+  write_lcd(0b0110,cmd[9], 1,INITIAL);
+  write_lcd(0b0111,cmd[10],4,INITIAL);
 
-  
   // write data
-  for(i=0;i<160;i++){
+  short i,j;
+  // 800*480/16=800*30
+  for(i=0;i<30;i++){
     for(j=0;j<800;j++){
-      // command
-      write_lcd(0b0110,DBL[14]);
-      // data
-      write_lcd(0b0111,(unsigned int)0xff00);
+      write_lcd(0b0110,cmd[14],1 ,SEND_DATA);
+      write_lcd(0b0111,data,   16,SEND_DATA);
     }
   }
-  for(i=0;i<160;i++){
-    for(j=0;j<800;j++){
-      // command
-      write_lcd(0b0110,DBL[14]);
-      // data
-      write_lcd(0b0111,(unsigned int)0x0ff0);
-    }
-  }
-  for(i=0;i<160;i++){
-    for(j=0;j<800;j++){
-      // command
-      write_lcd(0b0110,DBL[14]);
-      // data
-      write_lcd(0b0111,(unsigned int)0x00ff);
-    }
-  }
-
-
   return 0;
 }
